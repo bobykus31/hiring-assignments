f82e2536f722f27fd0636ddc679537649fd2d785
Ihor Piddubnyak <ihp@one.com>
2020-02-11 15:46:54 +0100

    tried on minikube

--- sre/dummy-pdf-or-png/main.go
+++ sre/dummy-pdf-or-png/main.go
@@ -11,19 +11,32 @@ func serveRandomFile(w http.ResponseWriter, r *http.Request) {
 	rnd := rand.Intn(10)
 	if rnd < 5 {
 		http.ServeFile(w, r, "./dummy.png")
+        log.Printf("Received request for dummy.png")
 		return
 	}
 	if rnd < 9 {
 		http.ServeFile(w, r, "./dummy.pdf")
+        log.Printf("Received request for dummy.pdf")
 		return
 	}
 	http.ServeFile(w, r, "./corrupt-dummy.pdf")
+    log.Printf("Received request for corrupt-dummy.pdf")
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
 }
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	http.HandleFunc("/", serveRandomFile)
-	err := http.ListenAndServe(":3000", nil)
+	http.HandleFunc("/health", healthHandler)
+	http.HandleFunc("/readiness", readinessHandler)
+	err := http.ListenAndServe(":3030", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
